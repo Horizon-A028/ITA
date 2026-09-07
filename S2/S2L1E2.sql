a2e2e01a0a8611d35d210229d85c932f38beea29
Horizon-A028 <marc.ponce.sochnov@gmail.com>
2026-09-07 20:32:18 +0200

    vault backup: 2026-09-07 20:32:18

---
+++ S2/S2L1E2.sql
@@ -0,0 +1,5 @@
+-- Utilitzant JOIN realitzaràs les següents consultes:
+
+-- Llistat dels països que estan generant vendes.
+-- Des de quants països es generen les vendes.
+-- Identifica la companyia amb la mitjana més gran de vendes.
