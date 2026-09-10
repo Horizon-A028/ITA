dbeff728fd1c07d6470dd1fa53740dc337732ac1
Horizon-A028 <marc.ponce.sochnov@gmail.com>
2026-09-10 10:05:11 +0200

    vault backup: 2026-09-10 10:05:11

--- S2/S2L2E2.sql
+++ S2/S2L2E2.sql
@@ -5,8 +5,45 @@
 -- 13 de març del 2024. 
 -- Ordena els resultats de major a menor quantitat.
 
-SELECT *
+-- Asumo que el valor se cuenta por transaccion, no la suma del dia:
+
+SELECT
+  c.name,
+  c.phone,
+  c.country,
+  DATE(t.instant) AS t_day,
+  t.amount
 FROM companies AS c
 JOIN transactions AS t
 ON t.company_id = c.id
-WHERE t.instant
+WHERE 
+  DATE(t.instant) in (DATE '2015-3-29', DATE '2018-7-20', DATE '2024-3-13')
+  AND t.amount BETWEEN 350 AND 400
+ORDER BY t.amount DESC
+;
+
+-- Asumo que el valor es la suma del dia:
+
+SELECT
+  c.name,
+  c.phone,
+  c.country,
+  DATE(t.instant) AS t_day,
+  SUM(t.amount) AS total
+FROM companies AS c
+JOIN transactions AS t
+ON t.company_id = c.id
+WHERE
+  DATE(t.instant) in (
+    DATE '2015-3-29',
+    DATE '2018-7-20',
+    DATE '2024-3-13'
+  )
+GROUP BY
+  c.name,
+  c.phone,
+  c.country,
+  t_day
+HAVING total BETWEEN 350 AND 400
+ORDER BY total DESC
+;
